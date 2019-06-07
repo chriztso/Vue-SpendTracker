e14d14505de40fd860317acf8f9832b84c816992
Chris Tso <christophertso2017@u.northwestern.edu>
2019-06-07 12:20:40 -0500

    Added login function and redirect to home

--- src/components/login.vue
+++ src/components/login.vue
@@ -2,13 +2,38 @@
   <div>
     <h1>Login Page</h1>
       <div class =  'fields'>
-        Username: <input type = 'text' placeholder = 'username'>
-        Password:  <input type = 'text' placeholder = 'password'>
+        Email: <input type = 'text' placeholder = 'email' v-model = 'email'>
+        Password:  <input type = 'text' placeholder = 'password' v-model = 'password'>
+        <input type = 'submit' value = "Sign In" v-on:click = 'loginUser'>
       </div>
       <p> Sign up for account <router-link to = '/signup'> here </router-link> </p>  
   </div>
 </template>
 
+<script>
+import firebase from 'firebase';
+
+export default{
+    data(){
+        return {
+            email: '', 
+            password : ''
+        }
+    }, 
+    methods : {
+      loginUser : function(){
+          firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+          .then(() => {
+              alert('You are logged in!');
+              this.$router.push({name : "Home"})
+          })
+          .catch((err) => {
+              alert('Error is' + err)
+          })
+      }
+    }
+}
+</script>
 
 <style>
 .fields{
