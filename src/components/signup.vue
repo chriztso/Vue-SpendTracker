ce9ba8b99af5b93bbf0a4172c4509468ba36a6e9
Chris Tso <christophertso2017@u.northwestern.edu>
2019-06-07 11:47:19 -0500

    Edited to sign up with email and added create user method

--- src/components/signup.vue
+++ src/components/signup.vue
@@ -1,11 +1,35 @@
 <template>
   <div class = 'signup'>
     <h1> Create account </h1>
-      Username <input type = 'text' placeholder = 'username'> 
-      Password <input type = 'text' placeholder = 'password'>    
+      Email <input type = 'text' placeholder = 'username' v-model = 'email'> 
+      Password <input type = 'text' placeholder = 'password' v-model = 'password'>    
+      <input type = 'submit' value = 'Create account' v-on:click = 'createUser'>
   </div>
 </template>
 
+<script>
+import firebase from 'firebase';
+
+export default{
+    data() {
+        return {
+            email : '', 
+            password : ''
+        }
+    }, 
+    methods : {
+        createUser : function(){
+            firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+            .then((user) => {
+                alert('Your account has been created!')
+            })
+            .catch((err) => {
+                alert('Error : ' + err);
+            })
+        }
+    }
+}
+</script>
 
 <style>
 .signup{
