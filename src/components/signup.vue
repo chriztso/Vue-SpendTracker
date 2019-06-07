3ba38dd2a2a4a50e39e1727b4921aac41b59ee6b
Chris Tso <christophertso2017@u.northwestern.edu>
2019-06-07 10:16:58 -0500

    Created signup view

---
+++ src/components/signup.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class = 'signup'>
+    <h1> Create account </h1>
+      Username <input type = 'text' placeholder = 'username'> 
+      Password <input type = 'text' placeholder = 'password'>    
+  </div>
+</template>
+
+
+<style>
+.signup{
+    display: flex; 
+    flex-direction: column;
+}
+</style>
