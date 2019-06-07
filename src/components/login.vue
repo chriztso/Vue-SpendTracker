d8ccfa3d297e3abd16412acd393bdde3f546694d
Chris Tso <christophertso2017@u.northwestern.edu>
2019-06-07 10:03:18 -0500

    Created login view

---
+++ src/components/login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Login Page</h1>
+      <div class =  'fields'>
+        Username: <input type = 'text' placeholder = 'username'>
+        Password:  <input type = 'text' placeholder = 'password'>
+      </div>  
+  </div>
+</template>
+
+
+<style>
+.fields{
+    display: flex; 
+    flex-direction: column;
+    width: 400px;
+    position: relative; 
+    left: 500px;
+}
+</style>
+
