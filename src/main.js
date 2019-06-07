255f59454dee8da5853cea5d883fe5181382db99
Chris Tso <christophertso2017@u.northwestern.edu>
2019-06-07 11:19:18 -0500

    Initialized firebase

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router.js'
+import firebase from 'firebase';
+import config from './components/firebase.config';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+firebase.initializeApp(config);
 
 new Vue({
   router,
